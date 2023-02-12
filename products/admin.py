48c66a7709f60de2899ab5229adf49c4a34554f2
KhamzinAdel <khamzin.adel@mail.ru>
2023-02-12 15:56:36 +0300

    get_image in product admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from modeltranslation.admin import TranslationAdmin
 
 from .models import Basket, Product, ProductCategory
@@ -8,20 +9,22 @@ from .models import Basket, Product, ProductCategory
 class ProductCategoryAdmin(TranslationAdmin):
     list_display = ('name', 'description')
     fields = ('name', 'description')
-    ordering = ('name', )
+    ordering = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(TranslationAdmin):
-    list_display = ('name', 'price', 'quantity', 'category')
-    fields = ('image', 'name', 'description', ('price', 'quantity'), 'stripe_product_price_id', 'category')
-    readonly_fields = ('description',)
+    list_display = ('name', 'price', 'quantity', 'category', 'get_image')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'stripe_product_price_id', 'category', 'get_image')
+    readonly_fields = ('description', 'get_image')
     search_fields = ('name',)
     ordering = ('name',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
 
 
 class BasketAdmin(admin.TabularInline):
     model = Basket
     fields = ('product', 'quantity')
     extra = 0
-
