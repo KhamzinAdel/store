ad8caca136b86fafa5f8a4d0fbe4cf123bd725e2
KhamzinAdel <khamzin.adel@mail.ru>
2023-03-14 13:12:04 +0300

    pagination product_list

--- api/views.py
+++ api/views.py
@@ -1,9 +1,17 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
 
+class ProductListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
 class ProductListAPIView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductListPagination
