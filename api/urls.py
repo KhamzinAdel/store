d3ef281f565dc0b3ff0d1b91c4fac6f9631e67c1
KhamzinAdel <khamzin.adel@mail.ru>
2023-03-16 08:40:11 +0300

    product_list api

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ app_name = 'api'
 
 urlpatterns = [
     path('productlist/', views.ProductListAPIView.as_view(), name='product_list'),
+    path('productlist/<int:category_id>', views.ProductListAPIView.as_view(), name='product_category'),
+    path('product/<int:pk>', views.ProductDetailAPIView.as_view(), name='product_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from products.models import Product
 from products.serializers import ProductSerializer
+from products.services import cashes_product
 
 
 class ProductListPagination(PageNumberPagination):
@@ -12,6 +16,19 @@ class ProductListPagination(PageNumberPagination):
 
 
 class ProductListAPIView(ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductListPagination
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        queryset = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+        products = cashes_product('products', queryset, 30)
+        return products
+
+
+class ProductDetailAPIView(APIView):
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializers = ProductSerializer(product)
+        return Response(serializers.data)
