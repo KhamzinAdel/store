a871178ea19de2afb7113d55c0e589aa8a09205d
KhamzinAdel <khamzin.adel@mail.ru>
2023-03-22 00:00:32 +0300

    permissions review

--- users/permissions.py
+++ users/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrStaffOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.user == request.user
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            obj.user == request.user or request.user.is_staff)
