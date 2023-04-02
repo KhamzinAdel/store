a5b8d72fd32a00d1df4ec9b393d418ff8392d795
KhamzinAdel <khamzin.adel@mail.ru>
2023-04-02 22:28:35 +0300

    refactor django_structure

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,3 @@
-"""store URL Configuration
-"""
-
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
--- store/yasg.py
+++ store/yasg.py
@@ -8,8 +8,6 @@ schema_view = get_schema_view(
       title="Store API docs",
       default_version='v1',
       description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -17,7 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-   path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
