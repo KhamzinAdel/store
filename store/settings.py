cf9109de68d69e5a2b6d6742376c9c8bca431cc9
KhamzinAdel <khamzin.adel@mail.ru>
2023-05-06 20:33:49 +0300

    refactor nginx

--- store/settings.py
+++ store/settings.py
@@ -250,7 +250,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
